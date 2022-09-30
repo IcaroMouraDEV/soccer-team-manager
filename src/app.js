c731dff0ba263a847a21fa4cac9573d35c9a6153
Icaro Joel <icaromoura.programador@gmail.com>
2022-09-30 20:45:50 -0300

    API RESTful complete

--- src/app.js
+++ src/app.js
@@ -3,10 +3,6 @@ const express = require('express');
 
 const app = express();
 
-// src/app.js
-
-// const express = require('express');
-
 const teams = [
   {
     id: 1,
@@ -20,6 +16,43 @@ const teams = [
   },
 ];
 
-// ...
+app.use(express.json());
 
-module.exports = { app, teams, express };
+app.get('/', (_req, res) => res.status(200).json({ message: 'Olá Mundo!' }));
+
+app.get('/teams', (_req, res) => res.status(200).json({ teams }));
+
+app.post('/teams', (req, res) => {
+  const newTeam = { ...req.body };
+  teams.push(newTeam);
+
+  res.status(201).json({ team: newTeam });
+});
+
+app.put('/teams/:id', (req, res) => {
+  const { id } = req.params;
+  const { name, initials } = req.body;
+  let updatedTeam;
+
+  for (let i = 0; i < teams.length; i += 1) {
+    const team = teams[i];
+
+    if (team.id === Number(id)) {
+      team.name = name;
+      team.initials = initials;
+      updatedTeam = team;
+    }
+  }
+
+  res.status(200).json({ updatedTeam });
+});
+
+app.delete('/teams/:id', (req, res) => {
+  const { id } = req.params;
+  const arrayPosition = teams.findIndex((team) => team.id === Number(id));
+  teams.splice(arrayPosition, 1);
+
+  res.status(200).end();
+});
+
+module.exports = app;
